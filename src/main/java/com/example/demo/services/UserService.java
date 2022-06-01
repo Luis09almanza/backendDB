eab330c699c644239d06adecf1005259d1972af6
Luis Hernandez <luis09almanza@gmail.com>
2022-06-01 14:03:38 -0600

    feat: Add the methods delete, post, getById and put

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -11,11 +11,35 @@ import java.util.List;
 public class UserService {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository userRepository; // Maneja Repositorio, para operaciones CRUD
 
 
+    //Listar los objetos de la base de datos
     public List<UserEntity> find(){
         return userRepository.findAll();
+    }
+
+    public UserEntity findById(Integer id){
+        return userRepository.findById(id).get();
+    }
+
+
+    //Metodo para guardar los datos en la base de datos
+    public UserEntity save(UserEntity userEntity){
+        return userRepository.save(userEntity);
+    }
+
+    public UserEntity update( Integer id, UserEntity userEntity){
+        UserEntity updatedUserEntity = userEntity;
+        updatedUserEntity.setId(id);
+
+        return userRepository.save(updatedUserEntity);
+
+    }
+
+    //Metodo para eliminar un registro de la tabla
+    public void deleteById(Integer id){
+        userRepository.deleteById(id);
     }
 
 }
